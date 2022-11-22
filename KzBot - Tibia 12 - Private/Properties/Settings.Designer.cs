17460a05a1d3a06ba5a752fc4520ef882c0d1598
Ismael <ismaelfesilva@gmail.com>
2022-11-22 00:02:27 -0300

    changed default key values

--- KzBot - Tibia 12 - Private/Properties/Settings.Designer.cs
+++ KzBot - Tibia 12 - Private/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace KzBot.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,7 @@ namespace KzBot.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("116")]
         public int Loot_Key {
             get {
                 return ((int)(this["Loot_Key"]));
@@ -37,7 +37,7 @@ namespace KzBot.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("35")]
         public int Target_Next_Key {
             get {
                 return ((int)(this["Target_Next_Key"]));
@@ -49,7 +49,7 @@ namespace KzBot.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("121")]
         public int Haste_Key {
             get {
                 return ((int)(this["Haste_Key"]));
@@ -61,7 +61,7 @@ namespace KzBot.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("123")]
         public int Utito_Key {
             get {
                 return ((int)(this["Utito_Key"]));

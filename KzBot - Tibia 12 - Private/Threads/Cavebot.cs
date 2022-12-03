364cc6953be7518d9f55627a18903496b4e90bc2
Ismael <ismaelfesilva@gmail.com>
2022-12-02 21:32:28 -0300

    added min lvl to check cap

--- KzBot - Tibia 12 - Private/Threads/Cavebot.cs
+++ KzBot - Tibia 12 - Private/Threads/Cavebot.cs
@@ -238,7 +238,9 @@ namespace KzBot.Threads
                         Globals.WaypointId = Globals.Config.Waypoints.FindIndex(w => w.Label.Trim() == waypoint.Extra.Trim());
                         break;
                     case WaypointType.Check_Cap:
-                        if (Objects.Player.Cap < int.Parse(extraData[0]))
+                        if (Objects.Player.Level < Globals.Config.min_Level_To_Check_Cap)
+                            Globals.WaypointId++;
+                        else if (Objects.Player.Cap < int.Parse(extraData[0]))
                             Globals.WaypointId = Globals.Config.Waypoints.FindIndex(w => w.Label == extraData[1].Trim());
                         else
                             Globals.WaypointId++;   
